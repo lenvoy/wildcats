718e6f36bea9347c93b2063c899f79363b21ee0e
Ben Levinson (RIT Student) <bml7725@ad.rit.edu>
2016-09-30 19:19:08 -0400

    Pushing new features; cylinders respond to movement with xbox controllers, collide together, gravity implemented, etc.

---
+++ GDD2 Project 2/Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 movementVector;
+    private Vector3 rotationVector;
+    private float movementSpeed = 4;
+    public int joystickNum;
+    private float rotSpeed = 180;
+    private float gravity = 9.81f;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        string joystickString = joystickNum.ToString();
+        movementVector.x = Input.GetAxis("L_XAxis_" + joystickString) * movementSpeed;
+        movementVector.z = Input.GetAxis("L_YAxis_" + joystickString) * movementSpeed;
+        movementVector.y -= gravity * Time.deltaTime;
+        transform.Rotate(0, Input.GetAxis("R_XAxis_" + joystickString) * rotSpeed * Time.deltaTime, 0);
+        transform.Translate(movementVector.x * Time.deltaTime, 0, movementVector.z * Time.deltaTime);
+
+    }
+
+    void onCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "egg")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
